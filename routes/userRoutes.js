0bd48dd51ad14e1a353c670f57643e95fe28ab69
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2024-07-05 09:59:16 -0300

    feature: add new routes to user functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,4 +6,7 @@ const userController = new UserController();
 
 router.get('/api/users', userController.getUsers.bind(userController));
 router.get('/api/users/:user_id/tasks', userController.getTasksByUser.bind(userController));
+router.post('/api/users', userController.addUser.bind(userController));
+router.put('/api/users/:id', userController.updateUser.bind(userController));
+router.delete('/api/users/:id', userController.deleteUser.bind(userController));
 module.exports = router;
